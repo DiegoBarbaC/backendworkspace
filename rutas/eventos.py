f151af2996239fe3868a43b78028e68d8cef7ec7
DiegoBarbaC <diego.barba011010@gmail.com>
2025-01-02 15:37:13 -0600

    endpoints de eventos agregados

--- rutas/eventos.py
+++ rutas/eventos.py
@@ -87,3 +87,65 @@ def getEvents():
         print("Error al obtener eventos:", str(e))
         return jsonify({"msg": "Error al obtener los eventos"}), 500
 
+#Ruta para eliminar un evento
+@eventos_bp.route('/deleteEvent/<event_id>', methods=['DELETE'])
+@jwt_required()  # Requiere autenticación
+def deleteEvent(event_id):
+    # Obtener el usuario actual
+    current_user = get_jwt_identity()
+    user = mongo.db.usuarios.find_one({"_id": ObjectId(current_user)})
+    
+    # Verificación de permisos
+    if not user or not (user.get("editar", False) or user.get("admin", False)):
+        return jsonify({"msg": "Acceso denegado: se requiere el permiso de edición"}), 403
+
+    # Eliminar el evento
+    result = mongo.db.eventos.delete_one({"_id": ObjectId(event_id)})
+    
+    if result.deleted_count == 1:
+        return jsonify({"msg": "Evento eliminado correctamente"}), 200
+    else:
+        return jsonify({"msg": "Error al eliminar el evento"}), 400
+
+#Ruta para actualizar un evento
+@eventos_bp.route('/updateEvent/<event_id>', methods=['PUT'])
+@jwt_required()  # Requiere autenticación
+def updateEvent(event_id):
+    # Obtener el usuario actual
+    current_user = get_jwt_identity()
+    user = mongo.db.usuarios.find_one({"_id": ObjectId(current_user)})
+    
+    # Verificación de permisos
+    if not user or not (user.get("editar", False) or user.get("admin", False)):
+        return jsonify({"msg": "Acceso denegado: se requiere el permiso de edición"}), 403
+
+    # Obtener los datos de la solicitud
+    data = request.form
+    titulo = data.get('titulo')
+    descripcion = data.get('descripcion')
+    fechaInicio = data.get('fechaInicio')
+    fechaFin = data.get('fechaFin')
+    usuarios = request.form.getlist('usuarios')
+
+    # Construir un diccionario solo con los campos a actualizar
+    update_data = {}
+    if titulo:
+        update_data['titulo'] = titulo
+    if descripcion:
+        update_data['descripcion'] = descripcion
+    if fechaInicio:
+        update_data['fechaInicio'] = fechaInicio
+    if fechaFin:
+        update_data['fechaFin'] = fechaFin
+    if usuarios:
+        update_data['usuarios'] = usuarios
+
+    # Actualizar el evento
+    result = mongo.db.eventos.update_one({"_id": ObjectId(event_id)}, {"$set": update_data})
+
+    if result.modified_count == 1:
+        return jsonify({"msg": "Evento actualizado correctamente"}), 200
+    else:
+        return jsonify({"msg": "Error al actualizar el evento"}), 400
+
+    
